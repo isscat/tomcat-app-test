ddd0b50f5807edef9660adc2061bb83c7cf6c8de
Indira Yarnagula <indhu.sony777@gmail.com>
2024-06-01 00:02:52 +0530

    initial commit

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet("/helloworld")
 public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
